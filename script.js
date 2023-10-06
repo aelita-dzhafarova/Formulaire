dc94ce5115d655d24152bf31a80fc9716510f44b
aelita-dzhafarova <aelita.dzhafarova@gmail.com>
2023-10-06 23:48:32 +0200

    work on screen1 JS

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('connecté !');
+// DEBUT ECRAN 1 - ECRAN EMPLOYEUR
+
+const btnScreen1 = document.querySelector('.button_screen1');
+
+const listScreen1 = document.querySelector('li:first-child');
+
+const listScreen2 = document.querySelector('.list-screen2');
+
+const screen1 = document.querySelector('.screen1');
+
+const screen2 = document.querySelector('.screen2');
+
+const divScreen1 = document.querySelector('.screen1');
+const inputsScreen1 = divScreen1.querySelectorAll('input');
+let allFilled = true;
+
+btnScreen1.addEventListener('click', function(){
+    
+    inputsScreen1.forEach(function(input) {
+        if (input.value === '') {
+            allFilled = false;
+            console.log('le champ' + input.value + 'est vide');
+        }
+    });
+
+    if (allFilled) {
+        listScreen1.style.color = "#6511D0";
+        listScreen2.style.color = "#E25C33";
+        screen1.style.display = "none";
+        screen2.style.display = "block";
+    } else {
+        alert('Tous les champs doivent être remplis.');
+    }
+});
+
+// FIN ECRAN 1 - ECRAN EMPLOYEUR
