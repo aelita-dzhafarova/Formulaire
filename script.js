e6052413014a5477ffddfaf1b4f0891d50dc1845
aelita-dzhafarova <aelita.dzhafarova@gmail.com>
2023-10-09 23:11:10 +0200

    work on screen6

--- script.js
+++ script.js
@@ -366,7 +366,7 @@ const listScreen6 = document.querySelector('.list-screen6');
 
 const screen6 = document.querySelector('.screen6');
 
-const inputsScreen5 = screen5.querySelectorAll('.required');
+const inputsScreen5 = screen5.querySelectorAll('.required5');
 
 // Pour revenir à l'écran 4
 
@@ -377,30 +377,29 @@ const btnBackToScreen4 = document.querySelector('.button_backscreen4');
 // Pour faire disparaître le cinquième écran et faire apparaître le sixième
 
 btnToScreen6.addEventListener('click', function(){
-    listScreen5.style.color = "#6511D0";
-    listScreen6.style.color = "#E25C33";
-    screen5.style.display = "none";
-    screen6.style.display = "block";
+    // inputsScreen5.forEach(function(input) {
+    //     if (input.value === '') {
+    //         allFilled = false;
+    //     }
+    // });
+
+    // if (allFilled) {
+        listScreen5.style.color = "#6511D0";
+        listScreen6.style.color = "#E25C33";
+        screen5.style.display = "none";
+        screen6.style.display = "block";
+    // } else {
+    //     alert('Tous les champs doivent être remplis.');
+    // }
 });
 
 // Pour revenir à l'écran 4
 
 btnBackToScreen4.addEventListener('click', function(){
-    inputsScreen5.forEach(function(input) {
-        if (input.value === '') {
-            allFilled = false;
-            console.log('le champ' + input.value + 'est vide');
-        }
-    });
-
-    if (allFilled) {
-        listScreen5.style.color = "#6511D0";
-        listScreen4.style.color = "#E25C33";
-        screen5.style.display = "none";
-        screen4.style.display = "block";
-    } else {
-        alert('Tous les champs doivent être remplis.');
-    }
+    listScreen5.style.color = "#6511D0";
+    listScreen4.style.color = "#E25C33";
+    screen5.style.display = "none";
+    screen4.style.display = "block";
 });
 
 // FIN ECRAN 5 - ECRAN LIEU DE TRAVAIL 
@@ -409,7 +408,255 @@ btnBackToScreen4.addEventListener('click', function(){
 
 // DEBUT ECRAN 6 - ECRAN FONCTIONS
 
+// Bloc Domaine Enfant
+
 // Variables
+
+const yesChildCare = document.getElementById('yes_child-care');
+const noChildCare = document.getElementById('no_child-care');
+
+const divFunctionChild1 = document.querySelector('.function_child1');
+
+// Linge enfant
+
+const yesChildClothes = document.getElementById('yes_child-clothes');
+const noChildClothes = document.getElementById('no_child-clothes');
+
+const divFunctionChild2 = document.querySelector('.function_child2');
+
+const divYesChildClothes = document.querySelector('.yes_child-clothes');
+
+// Devoir enfant
+
+const yesHomework = document.getElementById('yes_child-homework');
+const noHomework = document.getElementById('no_child-homework');
+
+const divFunctionChild3 = document.querySelector('.function_child3');
+
+const divYesHomework = document.querySelector('.yes_child-homework');
+
+// Nettoyer les especes de vie enfant
+
+const yesChildCleaning = document.getElementById('yes_child-cleaning');
+const noChildCleaning = document.getElementById('no_child-cleaning');
+
+const divFunctionChild4 = document.querySelector('.function_child4');
+
+const divYesChildCleaning = document.querySelector('.yes_child-cleaning');
+
+// S'occuper de plusieurs enfants
+
+const yesChildren = document.getElementById('yes_children');
+const noChildren = document.getElementById('no_children');
+
+const divYesChildren = document.querySelector('.yes_children');
+const divNoChildren = document.querySelector('.no_children');
+
+// Fonctions
+
+yesChildCare.addEventListener('click', function(){
+    divFunctionChild1.style.display = "block";
+});
+
+noChildCare.addEventListener('click', function(){
+    divFunctionChild1.style.display = "none";
+    divFunctionChild2.style.display = "none";
+    divFunctionChild3.style.display = "none";
+    divFunctionChild4.style.display = "none";
+});
+
+// Linge enfant
+
+yesChildClothes.addEventListener('click', function(){
+    divYesChildClothes.style.display = "block";
+    divFunctionChild2.style.display = "none";
+    divFunctionChild3.style.display = "none";
+    divFunctionChild4.style.display = "none";
+});
+
+noChildClothes.addEventListener('click', function(){
+    divFunctionChild2.style.display = "block";
+    divYesChildClothes.style.display = "none";
+});
+
+// Devoir enfant
+
+yesHomework.addEventListener('click', function(){
+    divYesHomework.style.display = "block";
+    divFunctionChild3.style.display = "none";
+    divFunctionChild4.style.display = "none";
+});
+
+noHomework.addEventListener('click', function(){
+    divYesHomework.style.display = "none";
+    divFunctionChild3.style.display = "block";
+});
+
+// Nettoyer les especes de vie enfant
+
+yesChildCleaning.addEventListener('click', function(){
+    divYesChildCleaning.style.display = "block";
+    divFunctionChild4.style.display = "none";
+});
+
+noChildCleaning.addEventListener('click', function(){
+    divYesChildCleaning.style.display = "none";
+    divFunctionChild4.style.display = "block";
+});
+
+// S'occuper de plusieurs enfants
+
+yesChildren.addEventListener('click', function(){
+    divYesChildren.style.display = "block";
+    divNoChildren.style.display = "none";
+});
+
+noChildren.addEventListener('click', function(){
+    divYesChildren.style.display = "none";
+    divNoChildren.style.display = "block";
+});
+
+// Bloc Domaine Adult
+
+// Variables
+
+const yesAdultCare = document.getElementById('yes_adult-care');
+const noAdultCare = document.getElementById('no_adult-care');
+
+const divFunctionAdult1 = document.querySelector('.function_adult1');
+
+// Réaliser les gestes liés à la délégation des soins d’un employeur en situation de handicap
+
+const yesDisabled1 = document.getElementById('yes_disabled1');
+const noDisabled1 = document.getElementById('no_disabled1');
+
+const divFunctionAdult2 = document.querySelector('.function_adult2');
+
+const divYesDisabled1 = document.querySelector('.yes_disabled1');
+
+// ASSISTER : repas, hygiène, habillage
+
+const yesDisabled2 = document.getElementById('yes_disabled2');
+const noDisabled2 = document.getElementById('no_disabled2');
+
+const divFunctionAdult3 = document.querySelector('.function_adult3');
+
+const divYesDisabled2 = document.querySelector('.yes_disabled2');
+
+// Réaliser à la place de l'employeur la préparation de repas spécifiques
+
+const yesDisabled3 = document.getElementById('yes_disabled3');
+const noDisabled3 = document.getElementById('no_disabled3');
+
+const divFunctionAdult4 = document.querySelector('.function_adult4');
+
+const divYesDisabled3 = document.querySelector('.yes_disabled3');
+
+// ACCOMPAGNER : repas, hygiène, habillage
+
+const yesDisabled4 = document.getElementById('yes_disabled4');
+const noDisabled4 = document.getElementById('no_disabled4');
+
+const divFunctionAdult5 = document.querySelector('.function_adult5');
+
+const divYesDisabled4 = document.querySelector('.yes_disabled4');
+
+// Effectuer et/ou accompagner l'employeur dans la préparation de repas spécifiques
+
+const yesDisabled5 = document.getElementById('yes_disabled5');
+const noDisabled5 = document.getElementById('no_disabled5');
+
+const divYesDisabled5 = document.querySelector('.yes_disabled5');
+const divNoDisabled5 = document.querySelector('.no_disabled5');
+
+// Fonctions
+
+yesAdultCare.addEventListener('click', function(){
+    divFunctionAdult1.style.display = "block";
+});
+
+noAdultCare.addEventListener('click', function(){
+    divFunctionAdult1.style.display = "none";
+    divFunctionAdult2.style.display = "none";
+    divFunctionAdult3.style.display = "none";
+    divFunctionAdult4.style.display = "none";
+    divFunctionAdult5.style.display = "none";
+});
+
+// Réaliser les gestes liés à la délégation des soins d’un employeur en situation de handicap
+
+yesDisabled1.addEventListener('click', function(){
+    divYesDisabled1.style.display = "block";
+    divFunctionAdult2.style.display = "none";
+    divFunctionAdult3.style.display = "none";
+    divFunctionAdult4.style.display = "none";
+    divFunctionAdult5.style.display = "none";
+});
+
+noDisabled1.addEventListener('click', function(){
+    divFunctionAdult2.style.display = "block";
+    divYesDisabled1.style.display = "none";
+});
+
+// ASSISTER : repas, hygiène, habillage
+
+yesDisabled2.addEventListener('click', function(){
+    divYesDisabled2.style.display = "block";
+    divFunctionAdult3.style.display = "none";
+    divFunctionAdult4.style.display = "none";
+    divFunctionAdult5.style.display = "none";
+});
+
+noDisabled2.addEventListener('click', function(){
+    divYesDisabled2.style.display = "none";
+    divFunctionAdult3.style.display = "block";
+});
+
+// Réaliser à la place de l'employeur la préparation de repas spécifiques
+
+yesDisabled3.addEventListener('click', function(){
+    divYesDisabled3.style.display = "block";
+    divFunctionAdult4.style.display = "none";
+    divFunctionAdult5.style.display = "none";
+});
+
+noDisabled3.addEventListener('click', function(){
+    divYesDisabled3.style.display = "none";
+    divFunctionAdult4.style.display = "block";
+});
+
+// ACCOMPAGNER : repas, hygiène, habillage
+
+yesDisabled4.addEventListener('click', function(){
+    divYesDisabled4.style.display = "block";
+    divFunctionAdult5.style.display = "none";
+});
+
+noDisabled4.addEventListener('click', function(){
+    divYesDisabled4.style.display = "none";
+    divFunctionAdult5.style.display = "block";
+});
+
+// Effectuer et/ou accompagner l'employeur dans la préparation de repas spécifiques
+
+yesDisabled5.addEventListener('click', function(){
+    divYesDisabled5.style.display = "block";
+    divNoDisabled5.style.display = "none";
+});
+
+noDisabled5.addEventListener('click', function(){
+    divYesDisabled5.style.display = "none";
+    divNoDisabled5.style.display = "block";
+});
+
+
+
+// Bloc Domaine Espace de vie
+
+const yesLifeSpace = document.getElementById('yes_lifespace');
+const noLifeSpace = document.getElementById('no_lifespace');
+
+const divFunctionsLifeSpace = document.querySelector('.functions_lifespace');
 
 // Pour faire disparaître le cinquième écran et faire apparaître le sixième
 
@@ -419,13 +666,11 @@ const listScreen7 = document.querySelector('.list-screen7');
 
 const screen7 = document.querySelector('.screen7');
 
-const inputsScreen6 = screen5.querySelectorAll('.required');
+const inputsScreen6 = screen6.querySelectorAll('.required6');
 
 // Pour revenir à l'écran 5
 
 const btnBackToScreen5 = document.querySelector('.button_backscreen5');
-
-// Fonctions
 
 // Pour faire disparaître le cinquième écran et faire apparaître le sixième
 
